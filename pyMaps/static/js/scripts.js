25650fcafcb69c1e6044ed337005344475dcba8a
Mike <miguelbq88@gmail.com>
2013-10-01 23:06:38 -0700

    tratar de poner todo global

--- pyMaps/static/js/scripts.js
+++ pyMaps/static/js/scripts.js
@@ -1,15 +1,61 @@
-$(function(){
-	if (navigator.geolocation)
-	{
-		navigator.geolocation.getCurrentPosition(getCoords, getError);
-	} else {
-		initialize(13.30272,-87.194107);
+$(document).ready(function(){
+
+	// if (navigator.geolocation)
+	// {
+	// 	navigator.geolocation.getCurrentPosition(getCoords, getError);
+	// } else {
+	// 	initialize(13.30272,-87.194107);
+	// }
+
+	var lat;
+	var lng;
+	var map;
+	var markerOptions;
+	var geocoder = new google.maps.Geocoder();
+	    // Hacemos la petición indicando la dirección e invocamos la función
+	    // geocodeResult enviando todo el resultado obtenido
+	geocoder.geocode({ 'address': 'Mexicali'}, function(result){
+		lat = result[0].geometry.location.lb;
+		lng = result[0].geometry.location.mb;
+
+			var latlng = new google.maps.LatLng(lat,lng);
+
+	var mapSettings = {
+		center : latlng,
+		zoom : 15,
+		mapTypeId : google.maps.MapTypeId.ROADMAP
 	}
+
+	$('#mapa').height(500);
+	map = new google.maps.Map($('#mapa').get(0),mapSettings);
+
+	markerOptions = {
+		position : latlng,
+		map : map,
+		draggable : true,
+		title : 'Drag me to hell'
+	};
+	
+	var marker = new google.maps.Marker(markerOptions);
+
+	google.maps.event.addListener(marker,'position_changed', function(){
+		getMarkerCoords(marker);
+	});
+
+	google.maps.event.addListener(marker, "click", function() {
+    	obtenerDireccion(marker);
+    });
+  //      	var input = result[0].geometry.location.toUrlValue();
+		// var latlngStr = input.split(",", 2);
+		// lat = parseFloat(latlngStr[0]);
+		// lng = parseFloat(latlngStr[1]);
+	});
+
 
 	function getCoords(position)
 	{
-		var lat = position.coords.latitude;
-		var lng = position.coords.longitude;
+		lat = position.coords.latitude;
+		lng = position.coords.longitude;
 
 		initialize(lat, lng);	
 	}
@@ -21,26 +67,21 @@ $(function(){
 
 	function initialize(lat, lng)
 	{
-		var latlng = new google.maps.LatLng(lat,lng);
-		var mapSettings = {
-			center : latlng,
-			zoom : 12,
-			mapTypeId : google.maps.MapTypeId.ROADMAP
-		}
+		
 
-		$('#mapa').height(500);
-		map = new google.maps.Map($('#mapa').get(0),mapSettings);
+	}
 
-		var marker = new google.maps.Marker({
-			position : latlng,
-			map : map,
-			draggable : true,
-			title : 'Drag me to hell'
-		});
+	function obtenerDireccion(direccion)
+	{
+		var geocoder = new google.maps.Geocoder();
+		var coords = direccion.getPosition();
+		var latLong_mx = new google.maps.LatLng(coords.lat(), coords.lng());
 
-		google.maps.event.addListener(marker,'position_changed', function(){
-			getMarkerCoords(marker);
-		});
+	    geocoder.geocode({'latLng': latLong_mx, 'region': 'MX'}, function(results){
+	    	var location_type_mx = results[0].geometry.location_type;
+        	alert(results[0].formatted_address);
+       	 	
+	    });
 	}
 
 	function getMarkerCoords(marker)
@@ -63,4 +104,38 @@ $(function(){
 			}
 		},'json');
 	});
+
+
+	$('#buscar').on('click', function() {
+	    // Obtenemos la dirección y la asignamos a una variable
+	    var address = $('#direccion').val();
+	    // Creamos el Objeto Geocoder
+	    var geocoder = new google.maps.Geocoder();
+	    // Hacemos la petición indicando la dirección e invocamos la función
+	    // geocodeResult enviando todo el resultado obtenido
+	    geocoder.geocode({ 'address': address}, geocodeResult);
+	});
+
+	function geocodeResult(results, status) {
+    // Verificamos el estatus
+    if (status == 'OK') {
+        // Si hay resultados encontrados, centramos y repintamos el mapa
+        // esto para eliminar cualquier pin antes puesto
+        var mapOptions = {
+            center: results[0].geometry.location,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map($("#mapa").get(0), mapOptions);
+        // fitBounds acercará el mapa con el zoom adecuado de acuerdo a lo buscado
+        map.fitBounds(results[0].geometry.viewport);
+        // Dibujamos un marcador con la ubicación del primer resultado obtenido
+        //var markerOptions = { position: results[0].geometry.location , draggable : true }
+        marker = new google.maps.Marker(markerOptions);
+        marker.setMap(map);
+    } else {
+        // En caso de no haber resultados o que haya ocurrido un error
+        // lanzamos un mensaje con el error
+        alert("Geocoding no tuvo éxito debido a: " + status);
+    }
+}
 });
